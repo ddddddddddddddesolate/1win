ad40d0156e6411728c8de7143c7c242b62d8afac
Alexander <aleksandr.kalashnik@purrweb.com>
2023-06-12 17:28:16 +0600

    add gif instead of retry button, update video

--- src/components/Video/Video.tsx
+++ src/components/Video/Video.tsx
@@ -11,21 +11,11 @@ const Video = ({ onVideoEnded }: Props) => {
 
   const [muted, setMuted] = useState<boolean>(true);
   const [isLoading, setIsLoading] = useState<boolean>(true);
-  const [isRestartButtonVisible, setIsRestartButtonVisible] = useState<boolean>(false);
+  const [isVideoEnded, setIsVideoEnded] = useState<boolean>(false);
 
   const unmuteVideo = useCallback(() => {
     setMuted(previousState => !previousState);
   }, []);
-
-  const restartVideo = useCallback(() => {
-    if (videoRef.current) {
-      (videoRef.current as HTMLVideoElement)
-        .play()
-        .catch(console.error);
-    }
-
-    setIsRestartButtonVisible(false);
-  }, [videoRef]);
 
   useEffect(() => {
     if (videoRef.current) {
@@ -39,35 +29,37 @@ const Video = ({ onVideoEnded }: Props) => {
         <div className={styles.loading}>Loading...</div>
       )}
 
-      <div className={styles.overlay}>
-        <video
-          ref={videoRef as any}
-          className={styles.video}
-          onLoadedData={() => {
-            setIsLoading(false);
-          }}
-          onEnded={() => {
-            onVideoEnded();
+      {isVideoEnded ? (
+        <img
+          src={`${process.env.PUBLIC_URL}/images/gif.gif`}
+          className={styles.gif}
+          alt="1win"
+        />
+      ) : (
+        <>
+          <video
+            ref={videoRef as any}
+            className={styles.video}
+            onLoadedData={() => {
+              setIsLoading(false);
+            }}
+            onEnded={() => {
+              onVideoEnded();
 
-            setIsRestartButtonVisible(true);
-          }}
-          autoPlay
-          muted
-          playsInline
-        >
-          <source src={`${process.env.PUBLIC_URL}/videos/video.mp4`} type="video/mp4" />
-        </video>
+              setIsVideoEnded(true);
+            }}
+            autoPlay
+            muted
+            playsInline
+          >
+            <source src={`${process.env.PUBLIC_URL}/videos/video.mp4`} type="video/mp4" />
+          </video>
 
-        {isRestartButtonVisible && (
-          <div className={styles.restart}>
-            <span onClick={restartVideo} className={styles.icon} />
-          </div>
-        )}
-
-        {(muted && !isRestartButtonVisible) && (
-          <div className={styles.unmute} onClick={unmuteVideo}>Unmute</div>
-        )}
-      </div>
+          {(muted && !isVideoEnded) && (
+            <div className={styles.unmute} onClick={unmuteVideo}>Unmute</div>
+          )}
+        </>
+      )}
     </div>
   );
 };
